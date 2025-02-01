4a24afb3b22d25ef9b45f05a5589720029a421d6
TomasP <perezgantomas@gmail.com>
2025-02-01 11:04:13 -0500

    initial screen added

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -31,7 +31,8 @@ export default function RootLayout() {
 
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
+      <Stack> 
+        <Stack.Screen name='index' options={{}} />
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         <Stack.Screen name="+not-found" />
       </Stack>
---
+++ app/index.tsx
@@ -0,0 +1,19 @@
+import { Image, StyleSheet, Platform, Button } from 'react-native';
+import { useRouter } from 'expo-router';
+import { ThemedText } from '@/components/ThemedText';
+import { ThemedView } from '@/components/ThemedView';
+import { Link, Stack } from 'expo-router';
+import React from 'react';
+
+export default function LoginScreen(){
+
+    return (
+        <>
+        <Stack.Screen options={{ title: 'Welcome Page'}} />
+        <ThemedView>
+            <ThemedText>Login Screen</ThemedText>
+            <Link href="/(tabs)">Go to tabs</Link>
+        </ThemedView>
+        </>
+    );
+}
